20752a126fbb42c5f1c338f9f67bce88e3888ee6
root <root@10.0.2.15>
2018-08-23 03:41:01 -0400

    commit

---
+++ learn_chef_httpd/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: learn_chef_httpd
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+#
+package 'httpd'
+
+service 'httpd' do
+   action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+   source 'index.html.erb'
+end
